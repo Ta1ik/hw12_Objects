9ce12bbe1f33d002d2a533fcdb3eacf4e2ce50ab
Hw2 <Kucherenko.vv@mail.ru>
2022-10-27 00:25:13 +0800

    hw12 Object выполнена

--- src/Object.java
+++ src/Object.java
@@ -7,6 +7,5 @@ public class Object {
         roman.setPublishingYear(2018);
         System.out.println("Книга 1. Название: " + learnScada.getBookName() + ". Автор: " + authorForLearn.getAuthorName() + " " + authorForLearn.getAuthorSurname() + ". Год издания: " + learnScada.getPublishingYear());
         System.out.println("Книга 2. Название: " + roman.getBookName() + ". Автор: " + authorForRead.getAuthorName() + " " + authorForRead.getAuthorSurname() + ". Год издания: " + roman.getPublishingYear());
-
     }
 }
